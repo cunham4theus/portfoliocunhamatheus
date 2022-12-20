c5e82fd68f28acd52b08f2e274e049e784969bf1
cunham4theus <cunhamatheus.m4@gmail.com>
2022-12-20 16:02:43 -0300

    adjusts barba JS and menu techs.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,58 +1,62 @@
 function delay(n) {
   n = n || 2000;
   return new Promise((done) => {
-      setTimeout(() => {
-          done();
-      }, n);
+    setTimeout(() => {
+      done();
+    }, n);
   });
 }
 
 function pageTransition() {
   var tl = gsap.timeline();
-  tl.to(".loading-screen", {
-      duration: 0.8,
-      width: "100%",
-      left: "0%",
-      ease: "Expo.easeInOut",
+  tl.to('.loading-screen', {
+    duration: 0.8,
+    width: '100%',
+    left: '0%',
+    ease: 'Expo.easeInOut',
   });
 
-  tl.to(".loading-screen", {
-      duration: 1,
-      width: "100%",
-      left: "100%",
-      ease: "Expo.easeInOut",
-      delay: 0.3,
+  tl.to('.loading-screen', {
+    duration: 1,
+    width: '100%',
+    left: '100%',
+    ease: 'Expo.easeInOut',
+    delay: 0.3,
   });
-  tl.set(".loading-screen", { left: "-100%" });
+  tl.set('.loading-screen', { left: '-100%' });
 }
 
 function contentAnimation() {
   var tl = gsap.timeline();
-  tl.from(".animate-this", { duration: 0.4, y: 30, opacity: 0, stagger: 0.4, delay: 0 });
+  tl.from('.animate-this', {
+    duration: 0.4,
+    y: 30,
+    opacity: 0,
+    stagger: 0.4,
+    delay: 0,
+  });
 }
 
 $(function () {
   barba.init({
-      sync: true,
+    sync: true,
 
-      transitions: [
-          {
-              async leave(data) {
-                  const done = this.async();
+    transitions: {
+      async leave(data) {
+        const done = this.async();
 
-                  pageTransition();
-                  await delay(500);
-                  done();
-              },
+        pageTransition();
+        await delay(1000);
+        done();
+      },
 
-              async enter(data) {
-                  contentAnimation();
-              },
+      async enter(data) {
+        contentAnimation();
+      },
 
-              async once(data) {
-                  contentAnimation();
-              },
-          },
-      ],
+      async once(data) {
+        contentAnimation();
+      },
+    },
   });
 });
